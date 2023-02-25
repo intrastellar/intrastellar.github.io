ccbfd342b9ff13f7f006d793b3099caef81c5beb
unknown <darik.pc@gmail.com>
2023-02-25 13:43:06 +0100

    Fix names in ES

--- minify.bat
+++ minify.bat
@@ -1,6 +1,2 @@
-minifyhtml --output seo-form-complete.html --keep-closing-tags --minify-css --keep-html-and-head-opening-tags --ensure-spec-compliant-unquoted-attribute-values full_html\seo-form-complete.html
-minifyhtml --output seo-form-complete-es.html --keep-closing-tags --minify-css --keep-html-and-head-opening-tags --ensure-spec-compliant-unquoted-attribute-values full_html\seo-form-complete-es.html
-minifyhtml --output seo-form-es.html --keep-closing-tags --minify-css --keep-html-and-head-opening-tags --ensure-spec-compliant-unquoted-attribute-values full_html\seo-form-es.html
 minifyhtml --output es.html --keep-closing-tags --minify-css --keep-html-and-head-opening-tags --ensure-spec-compliant-unquoted-attribute-values full_html\es.html
 minifyhtml --output index.html --keep-closing-tags --minify-css --keep-html-and-head-opening-tags --ensure-spec-compliant-unquoted-attribute-values full_html\index.html
-minifyhtml --output seo-form.html --keep-closing-tags --minify-css --keep-html-and-head-opening-tags --ensure-spec-compliant-unquoted-attribute-values full_html\seo-form.html
---
+++ minify_forms.bat
@@ -0,0 +1,4 @@
+minifyhtml --output seo-form-complete.html --keep-closing-tags --minify-css --keep-html-and-head-opening-tags --ensure-spec-compliant-unquoted-attribute-values full_html\seo-form-complete.html
+minifyhtml --output seo-form-complete-es.html --keep-closing-tags --minify-css --keep-html-and-head-opening-tags --ensure-spec-compliant-unquoted-attribute-values full_html\seo-form-complete-es.html
+minifyhtml --output seo-form-es.html --keep-closing-tags --minify-css --keep-html-and-head-opening-tags --ensure-spec-compliant-unquoted-attribute-values full_html\seo-form-es.html
+minifyhtml --output seo-form.html --keep-closing-tags --minify-css --keep-html-and-head-opening-tags --ensure-spec-compliant-unquoted-attribute-values full_html\seo-form.html
